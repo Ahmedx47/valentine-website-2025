90218c13273a967c49c725e9db71b43ad8655b0b
Ahmedx47 <ahmed.damha911@gmail.com>
2026-02-11 04:27:56 +0500

    Personalize Valentine content and add media
    Update config.js to personalize the experience (valentineName -> "Begam", change main question date to Feb 13, 2025), revise love meter messages and celebration text, and fix config object ending. Modify index.html to add a floating gallery (styles + markup), wire local background music to media/yoursong.mp3, and clean up some inline comments. Add media assets (pic1.jpg, pic2.jpg, pic3.jpg, video1.mp4, video2.mp4, yoursong.mp3) to be used by the new gallery and audio player.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Begam",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine on February 13th, 2025? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "letsss gooo i love you too meri jaan",  // Shows when they go past 5000%
+        high: "i know you love me more than this",              // Shows when they go past 1000%
+        normal: "yai kya baat hoi aur karain"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "idhar ayien i have a surprise for you begam, and lots of warm hugs and a kiss for my bby!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
@@ -83,7 +83,6 @@ const CONFIG = {
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
     }
-};
-
+}
 // Don't modify anything below this line unless you know what you're doing
 window.VALENTINE_CONFIG = CONFIG; 
